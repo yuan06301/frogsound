d97c718ca295ef87243547aeba39b8f05cb16424
yuan06301 <134923630+yuan06301@users.noreply.github.com>
2023-05-29 17:39:49 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = ' https://yuan06301.github.io/frogsound/model.json';
 
 
 function preload() {
