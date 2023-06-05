fefecdfc97c0825df0673678750b1a97e95cffd6
yuan06301 <134923630+yuan06301@users.noreply.github.com>
2023-06-05 17:15:52 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -51,5 +51,5 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
-  ThunkableWebviewerExtension.postMessage();
+  ThunkableWebviewerExtension.postMessage(label);
 }
